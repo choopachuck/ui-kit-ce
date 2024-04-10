ae634a06a3c3555caf9824d10a72894b2bee4f4e
Denis Svyatovets <dosvyatovets@sberbank.ru>
2024-04-10 17:02:04 +0300

    fix(notification): исправлена работа limit при StrictMode [SBTSUPPORT-37758]

--- packages/notification/src/hooks/useNotificationContainer.ts
+++ packages/notification/src/hooks/useNotificationContainer.ts
@@ -41,141 +41,150 @@ export const useNotificationContainer = (
     return showingNotificationIds.includes(id)
   }
 
-  const clearWaitingQueue = () => {
-    if (propsRef.current.limit) {
-      notificationsCount -= notificationsQueue.length
-      notificationsQueue = []
+  React.useEffect(() => {
+    const clearWaitingQueue = () => {
+      if (propsRef.current.limit) {
+        notificationsCount -= notificationsQueue.length
+        notificationsQueue = []
+      }
     }
-  }
 
-  const appendNotification = (
-    content: React.ReactNode,
-    props: NotificationProps
-  ) => {
-    if (props.id) {
-      notificationByIdMap[props.id] = {
-        content,
-        props,
+    const appendNotification = (
+      content: React.ReactNode,
+      props: NotificationProps
+    ) => {
+      if (props.id) {
+        notificationByIdMap[props.id] = {
+          content,
+          props,
+        }
+
+        setShowingNotificationIds((prevState) => [
+          ...prevState,
+          props.id as string,
+        ])
+      }
+    }
+
+    const removeNotification = (notificationId: string) => {
+      setShowingNotificationIds((prevState) =>
+        prevState.filter((id) => id !== notificationId)
+      )
+    }
+
+    const removeAllNotifications = () => {
+      setShowingNotificationIds([])
+    }
+
+    const removeCompletelyFromStore = (notificationId: string) => {
+      delete notificationByIdMap[notificationId]
+      notificationsCount = (notificationsCount || 1) - 1
+
+      if (notificationsQueue.length) {
+        dequeueNotification()
+      } else {
+        forceUpdate()
+      }
+    }
+
+    const dequeueNotification = () => {
+      const notification = notificationsQueue.shift()
+      if (notification) {
+        appendNotification(notification.content, notification.props)
+      }
+    }
+
+    const createNotification = (
+      content: React.ReactNode,
+      options: NotificationProps
+    ) => {
+      const { id } = options
+
+      if (id && notificationByIdMap[id]) {
+        return
       }
 
-      setShowingNotificationIds((prevState) => [
-        ...prevState,
-        props.id as string,
-      ])
-    }
-  }
+      notificationsCount++
 
-  const removeNotification = (notificationId: string) => {
-    setShowingNotificationIds((prevState) =>
-      prevState.filter((id) => id !== notificationId)
-    )
-  }
+      const closeNotification = () => removeNotification(id as string)
 
-  const removeAllNotifications = () => {
-    setShowingNotificationIds([])
-  }
-
-  const removeCompletelyFromStore = (notificationId: string) => {
-    delete notificationByIdMap[notificationId]
-    notificationsCount = (notificationsCount || 1) - 1
-
-    if (notificationsQueue.length) {
-      dequeueNotification()
-    } else {
-      forceUpdate()
-    }
-  }
-
-  const dequeueNotification = () => {
-    const notification = notificationsQueue.shift()
-    if (notification) {
-      appendNotification(notification.content, notification.props)
-    }
-  }
-
-  const createNotification = (
-    content: React.ReactNode,
-    options: NotificationProps
-  ) => {
-    const { id } = options
-
-    if (id && notificationByIdMap[id]) {
-      return
-    }
-
-    notificationsCount++
-
-    const closeNotification = () => removeNotification(id as string)
-
-    const notificationProps: NotificationProps = {
-      ...options,
-      closeNotification,
-      position: options.position || propsRef.current.position,
-      classes: mergeClasses({
-        classes1: propsRef.current.notificationClasses || {},
-        classes2: options.classes || {},
-        mergeCallback: clsx,
-      }),
-      closeButtonAriaLabel:
-        options.closeButtonAriaLabel || propsRef.current.closeButtonAriaLabel,
-      closeOnEscapeKeyDown:
-        typeof options.closeOnEscapeKeyDown === 'boolean'
-          ? options.closeOnEscapeKeyDown
-          : propsRef.current.closeOnEscapeKeyDown,
-      pauseOnHover:
-        typeof options.pauseOnHover === 'boolean'
-          ? options.pauseOnHover
-          : propsRef.current.pauseOnHover,
-      pauseOnWindowBlur:
-        typeof options.pauseOnWindowBlur === 'boolean'
-          ? options.pauseOnWindowBlur
-          : propsRef.current.pauseOnWindowBlur,
-      closeOnClick:
-        typeof options.closeOnClick === 'boolean'
-          ? options.closeOnClick
-          : propsRef.current.closeOnClick,
-      autoClose:
-        options.autoClose !== undefined
-          ? options.autoClose
-          : propsRef.current.autoClose,
-      showCloseButton:
-        typeof options.showCloseButton === 'boolean'
-          ? options.showCloseButton
-          : propsRef.current.showCloseButton,
-      showIndicator:
-        typeof options.showIndicator === 'boolean'
-          ? options.showIndicator
-          : propsRef.current.showIndicator,
-      removeNotification: () => removeCompletelyFromStore(options.id as string),
-    }
-
-    let notificationContent = content
-
-    if (React.isValidElement(content) && typeof content.type !== 'string') {
-      notificationContent = React.cloneElement(content, {
+      const notificationProps: NotificationProps = {
+        ...options,
         closeNotification,
-        notificationProps,
-      })
+        position: options.position || propsRef.current.position,
+        classes: mergeClasses({
+          classes1: propsRef.current.notificationClasses || {},
+          classes2: options.classes || {},
+          mergeCallback: clsx,
+        }),
+        closeButtonAriaLabel:
+          options.closeButtonAriaLabel || propsRef.current.closeButtonAriaLabel,
+        closeOnEscapeKeyDown:
+          typeof options.closeOnEscapeKeyDown === 'boolean'
+            ? options.closeOnEscapeKeyDown
+            : propsRef.current.closeOnEscapeKeyDown,
+        pauseOnHover:
+          typeof options.pauseOnHover === 'boolean'
+            ? options.pauseOnHover
+            : propsRef.current.pauseOnHover,
+        pauseOnWindowBlur:
+          typeof options.pauseOnWindowBlur === 'boolean'
+            ? options.pauseOnWindowBlur
+            : propsRef.current.pauseOnWindowBlur,
+        closeOnClick:
+          typeof options.closeOnClick === 'boolean'
+            ? options.closeOnClick
+            : propsRef.current.closeOnClick,
+        autoClose:
+          options.autoClose !== undefined
+            ? options.autoClose
+            : propsRef.current.autoClose,
+        showCloseButton:
+          typeof options.showCloseButton === 'boolean'
+            ? options.showCloseButton
+            : propsRef.current.showCloseButton,
+        showIndicator:
+          typeof options.showIndicator === 'boolean'
+            ? options.showIndicator
+            : propsRef.current.showIndicator,
+        removeNotification: () =>
+          removeCompletelyFromStore(options.id as string),
+      }
+
+      let notificationContent = content
+
+      if (React.isValidElement(content) && typeof content.type !== 'string') {
+        notificationContent = React.cloneElement(content, {
+          closeNotification,
+          notificationProps,
+        })
+      }
+
+      const limit = propsRef.current.limit
+
+      if (limit && limit > 0 && notificationsCount > limit) {
+        notificationsQueue.push({
+          content: notificationContent,
+          props: notificationProps,
+        })
+      } else {
+        appendNotification(notificationContent, notificationProps)
+      }
     }
 
-    const limit = propsRef.current.limit
-
-    if (limit && limit > 0 && notificationsCount > limit) {
-      notificationsQueue.push({
-        content: notificationContent,
-        props: notificationProps,
-      })
-    } else {
-      appendNotification(notificationContent, notificationProps)
-    }
-  }
-
-  React.useEffect(() => {
     eventManager
       .on(Event.Show, createNotification)
       .on(Event.Remove, removeNotification)
       .on(Event.ClearAll, removeAllNotifications)
       .on(Event.ClearWaitingQueue, clearWaitingQueue)
+
+    return () => {
+      eventManager
+        .off(Event.Show, createNotification)
+        .off(Event.Remove, removeNotification)
+        .off(Event.ClearAll, removeAllNotifications)
+        .off(Event.ClearWaitingQueue, clearWaitingQueue)
+    }
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   const notificationsToRender: NotificationsToRender = {}
