658ea1e6040c3b74f78217a9789056cc2b0a556b
Ivan Iakovlev <out-yakovlev.i@sbertech.ru>
2024-06-26 18:01:38 +0300

    fix(dropdown-menu): позиционирование dropdown при первом рендере если ширина dropdown больше экрана

--- packages/dropdown-menu/src/constants/index.ts
+++ packages/dropdown-menu/src/constants/index.ts
@@ -13,6 +13,7 @@ export const modifiers: Exclude<DropdownProps['modifiers'], undefined> = [
     requiresIfExists: ['offset', 'flip'],
     fn: ({
       state,
+      instance,
       name,
       options,
     }: ModifierArguments<Record<string, unknown>>): void => {
@@ -25,7 +26,7 @@ export const modifiers: Exclude<DropdownProps['modifiers'], undefined> = [
       let maxWidth = innerWidth - 32
 
       //Костыль для вычесления оффсетов при первом рендере
-      const isBiggerThenViewport = state.rects.popper.width > innerWidth - 32
+      const isBiggerThenViewport = state.rects.popper.width > maxWidth
       const overflowLeft = isBiggerThenViewport ? -16 : overflow.left
       const overflowRight = isBiggerThenViewport ? -16 : overflow.right
 
@@ -37,6 +38,12 @@ export const modifiers: Exclude<DropdownProps['modifiers'], undefined> = [
       }
 
       state.modifiersData[name] = maxWidth
+
+      //так как state при первом рендере не учитывает модифаер maxWidth для расчёта позиции надо обновить инстанс
+      if (isBiggerThenViewport) {
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        instance.update()
+      }
     },
   },
   {
